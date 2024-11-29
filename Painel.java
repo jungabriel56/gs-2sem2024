4cc83f6478c7435306f71d4efcad436e3aa66b56
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-29 19:51:42 -0300

    primeiro commit

---
+++ Painel.java
@@ -0,0 +1,16 @@
+package br.com.fiap.jungabriel56.global_solution.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class Painel {
+    
+    
+    private Long id;
+    private String modelo;
+
+
+
+}
---
+++ src/test/java/br/com/fiap/jungabriel56/gs/GsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.jungabriel56.gs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
